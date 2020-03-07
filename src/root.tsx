229ad85ccc3f5ea0fcfd9e0732dc9cf822cd14f2
Kaio Duarte <duartedossantos@gmail.com>
2020-03-07 02:43:13 +0000

    add history and remove styled context

--- src/root.tsx
+++ src/root.tsx
@@ -1,29 +1,24 @@
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
-import { BrowserRouter, Route } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
+import { Router, Route } from 'react-router-dom';
 import { CssBaseline, MuiThemeProvider } from '@material-ui/core';
 import { ConfirmProvider } from 'material-ui-confirm';
 import { GlobalStyle } from './styles/global';
 import { theme } from './styles/theme';
-import { AuthProvider } from './contexts/auth';
 import App from './app';
+import history from './utils/history';
 
 function Root() {
   return (
     <MuiThemeProvider theme={theme}>
-      <ThemeProvider theme={theme}>
-        <ConfirmProvider>
-          <AuthProvider>
-            <CssBaseline />
-            <GlobalStyle />
+      <ConfirmProvider>
+        <CssBaseline />
+        <GlobalStyle />
 
-            <BrowserRouter>
-              <Route component={App} />
-            </BrowserRouter>
-          </AuthProvider>
-        </ConfirmProvider>
-      </ThemeProvider>
+        <Router history={history}>
+          <Route component={App} />
+        </Router>
+      </ConfirmProvider>
     </MuiThemeProvider>
   );
 }
